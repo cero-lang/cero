b0950570b1e1da35d5e701a2384cbfbeb96284c1
William Roy <wroy@proton.me>
2022-08-20 21:46:09 -0400

    Add source files lookup with the compiler driver
    Signed-off-by: William Roy <wroy@proton.me>

--- cero/main.cc
+++ cero/main.cc
@@ -1,27 +1,35 @@
 // SPDX-License-Identifier: Apache-2.0
 
-#include <cero-lexer/cero-lexer.hh>
+#include <span>
+#include <string_view>
+#include <fstream>
+#include <vector>
 
-#include <magic_enum.hpp>
-
-#include <iomanip>
-#include <iostream>
-
-auto operator<<(std::ostream &os, const Cero::Token::Kind &kind) -> std::ostream &
-{
-  return os << magic_enum::enum_name(kind);
-}
+using std::string_view;
+using std::span;
 
 auto main([[maybe_unused]] const int argc, [[maybe_unused]] char *argv[]) -> int
 {
-  const auto source = "auto";
+  for (std::string_view argument : std::span(argv, argc)) {
+    if (argument == "v" || argument == "version") { }
+    if (argument == "h" || argument == "help") { }
 
-  Cero::Lexer lex(source);
-  for (auto token = lex.next(); token.kind() != Cero::Token::Kind::End
-       && token.kind() != Cero::Token::Kind::Unexpected;
-       token = lex.next()) {
-    std::cout << token.kind() << " : " << token.string() << "\n";
+    std::ifstream read;
+    read.exceptions(read.exceptions() | std::ios::failbit);
+    try {
+      read.open(argument.data());
+    } catch (std::ios_base::failure &e) {
+      std::runtime_error error(e.what());
+    }
+
+    std::vector<std::string> source;
+    std::string line;
+    while (std::getline(read, line)) {
+      source.push_back(line);
+    }
+
+    // Lexer lexer(source);
+    // lexer.next();
   }
-
   return 0;
 }
